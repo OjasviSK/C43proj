0b54ba1aeac84d306109cb94577990801b7038d6
OjasviSK <80693311+OjasviSK@users.noreply.github.com>
2021-09-22 19:34:07 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "ELEVATOR";
+const accessCode2 = "RUDDER";
+const accessCode3 = "AILERON";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("E E L V T R A O", 100,50)
+    fill("lightblue")
+    text("Hint: Help in the pitching of the aeroplane !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("R D U E R D", 700,150)
+    fill("lightblue")
+    text("Hint: Help in the yawing of the aeroplane !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A L I R N O E", 100,250)
+    fill("lightblue")
+    text("Hint: Help in rolling of the aeroplane !!", 100,270)
+
+}
